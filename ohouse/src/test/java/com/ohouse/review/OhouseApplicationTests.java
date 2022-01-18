1d3e058adbeb4d86e550e2bda9b0808e02a38170
user <user@wex.test.com>
2022-01-18 11:03:09 +0900

    ds

---
+++ ohouse/src/test/java/com/ohouse/review/OhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohouse.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
