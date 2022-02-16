84e8a05003ff463cb2e7427372ae9c09a453e84c
Suyun-prog <71685378+Suyun-prog@users.noreply.github.com>
2022-02-16 17:42:30 +0900

    json 오류 해결

--- ohouse/src/main/webapp/js/KeyWord.js
+++ ohouse/src/main/webapp/js/KeyWord.js
@@ -8,40 +8,39 @@ draw("미엘레 1단 스탠드 행거 4colors");
 function draw(product){
 
 	var words = getApi(product);
+	console.log(words);
  	var width = 800;
 	var height = 800;
 	
-d3.layout.cloud()
-  	.size([width, height])
- 	.words(words)
-	.padding(5)
-  	.rotate(0)
-  	.font("Impact")
-  	.fontSize(function(d) { return d.weight;})
-  	.on("end", this.end)
-  	.start();
-
- 
- function drawcloud(words) {
-	d3.select("#wordcloud")
-      .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .append("g")
-      .attr("transform", "translate("+ width/2 +","+ height/2 +")")
-      .selectAll("text")
-      .data(words)
-      .enter()
-      .append("text")
-      .style("font-size", function(d) { return d.weight + "px"; })
-      .style("font-family", "Impact")
-      .attr("text-anchor", "middle")
-      .attr("transform", function(d) {
-          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; 
-      })
-      .text(function(d) { return d.label; })
-      }
-  }
+	
+	d3.layout.cloud()
+	  	.size([width, height])
+	 	.words(words)
+	 	.padding(0)
+	  	.rotate(0)
+	  	.font("Impact")
+	  	.fontSize(function(d) {return d.weight;})
+	  	.on("end", drawcloud)
+	  	.start();
+	  	
+	function drawcloud(words) {
+	
+		d3.select("#wordcloud").append("svg")
+	      .attr("width", width)
+	      .attr("height", height)
+	      .append("g")
+	      .attr("transform", "translate("+ width/2 +","+ height/2 +")")
+	      .selectAll("text")
+	      .data(words)
+	      .enter().append("text")
+	      .style("font-size", function(d) { return d.weight + "px"; })
+	      .attr("text-anchor", "middle")
+	      .attr("transform", function(d) {
+	          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; 
+	      })
+	      .text(function(d) { return d.label; });
+	    }
+}
 
 
 // API data 파싱
@@ -56,15 +55,15 @@ function getApi(product){
 			console.log(par);
 			var Json = par['es_apiResponse']['ibmsc_facet']['ibmsc_facetValue'];
 			console.log(Json);
-			for (idx in Json){0
+			for (idx in Json){
 				/*arr[idx] = {"label":Json[idx]['label'], "weight":Json[idx]['weight']}; */
 				arr.push({
 						label : Json[idx]['label'],
-						value : Json[idx]['weight']
+						weight : Json[idx]['weight']
 					});
 				}
 					console.log(arr);
-					return arr;
 				}
-			});			
-		}
+			});
+			return arr;		
+	}
