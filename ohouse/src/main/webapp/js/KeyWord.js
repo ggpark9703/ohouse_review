c5e438e38e8ea4c3a847d9fd87bd58153ccf4ad8
Suyun-prog <71685378+Suyun-prog@users.noreply.github.com>
2022-02-17 18:10:48 +0900

    b

--- ohouse/src/main/webapp/js/KeyWord.js
+++ ohouse/src/main/webapp/js/KeyWord.js
@@ -1,17 +1,34 @@
-
-// 초기화면
+//첫 화면
 $(document).ready(function(){
 draw("미엘레 1단 스탠드 행거 4colors");
 });
 
 
+//워드클라우드 구현
 function draw(product){
 
-	var words = getApi(product);
-	console.log(words);
- 	var width = 800;
-	var height = 800;
+/*	var words = getApi(product);
+	console.log(words);*/
 	
+var words = [{label: '좋다', value: 1.02}
+,{label: '너무', value: 1.09}
+,{label:'같다', value: 1.1}
+,{label: '가격', value: 1.92}
+,{label: '잘', value: 1.05}
+,{label: '예쁘다', value: 2.53}
+,{label: '이쁘다', value: 2.53}
+,{label: '하다', value: 1.01}
+,{label: '깔끔하다', value: 2.53}
+,{label: '있다', value: 1.02}
+,{label: '성비', value: 1.93}
+,{label: '디자인', value: 2.14}
+,{label: '튼튼하다', value: 1.04}
+,{label: '사다', value: 1.06}];
+console.log(words);
+	
+	var fill = d3.scale.category20();
+	var width = 1000;
+	var height = 1000;
 	
 	d3.layout.cloud()
 	  	.size([width, height])
@@ -19,10 +36,10 @@ function draw(product){
 	 	.padding(0)
 	  	.rotate(0)
 	  	.font("Impact")
-	  	.fontSize(function(d) {return d.weight;})
+	  	.fontSize(function(d) {return d.value*10;})
 	  	.on("end", drawcloud)
 	  	.start();
-	  	
+	  	 	
 	function drawcloud(words) {
 	
 		d3.select("#wordcloud").append("svg")
@@ -33,19 +50,40 @@ function draw(product){
 	      .selectAll("text")
 	      .data(words)
 	      .enter().append("text")
-	      .style("font-size", function(d) { return d.weight + "px"; })
+	      .style("font-size", function(d) { return d.value*10 + "px"; })
+   		.style("fill", function(d, i) { return fill(i); })
 	      .attr("text-anchor", "middle")
 	      .attr("transform", function(d) {
 	          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")"; 
 	      })
-	      .text(function(d) { return d.label; });
-	    }
+	      .text(function(d) { return d.label; })
+          .on("mouseover", handleMouseOver)
+          .on("mouseout", handleMouseOut)
+          .on("click", handleClick);
+          
+          function handleMouseOver(d, i) {
+          d3.select(this)
+            .classed("word-hovered", true)
+              .attr("font-weight", "bold");
+        }
+        
+        function handleMouseOut(d, i) {
+          d3.select(this)
+            .classed("word-hovered", false)
+            .attr("font-weight", "normal");
+        }
+        
+        function handleClick(d, i) {
+          var e = d3.select(this);
+
+        }
+ 
 }
 
-
+}
 // API data 파싱
 function getApi(product){
-		var arr = new Array();
+		var arr = [];
 		$.ajax({
 		type: "GET",
 		url: "/wpi",
@@ -56,14 +94,19 @@ function getApi(product){
 			var Json = par['es_apiResponse']['ibmsc_facet']['ibmsc_facetValue'];
 			console.log(Json);
 			for (idx in Json){
-				/*arr[idx] = {"label":Json[idx]['label'], "weight":Json[idx]['weight']}; */
+				if(Json[idx]['es_property'][0]['value']<1.0)
+				{
+					idx = idx + 1;
+				}else{
 				arr.push({
 						label : Json[idx]['label'],
-						weight : Json[idx]['weight']
+						value : Math.floor(Json[idx]['es_property'][0]['value']*100)/100
 					});
+					}
 				}
-					console.log(arr);
+					
 				}
-			});
-			return arr;		
-	}
+			})
+			console.log(arr);
+			return arr;
+			}
