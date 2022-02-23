da033af3896841b0b0353960f70f8b0edad2cc14
Suyun-prog <71685378+Suyun-prog@users.noreply.github.com>
2022-02-23 17:48:16 +0900

    분석페이지(디자인 미적용)

--- ohouse/src/main/java/com/ohouse/review/service/KeywordServiceImpl.java
+++ ohouse/src/main/java/com/ohouse/review/service/KeywordServiceImpl.java
@@ -2,8 +2,10 @@ package com.ohouse.review.service;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.nio.charset.Charset;
 
 import org.springframework.stereotype.Service;
@@ -19,7 +21,7 @@ public class KeywordServiceImpl implements KeywordService {
 		InputStreamReader in = null;
 		
 		// product 부분을 변수로 받음
-		String myURL = "http://10.10.0.4:8393/api/v10/search/facets?collection=T1_OZIP2022&output=application/json&facet={%22namespace%22:%22keyword%22,%22count%22:%22100%22,%22id%22:%22$._word%22}&query=(keyword::/추천/디자인%20OR%20keyword::/추천/가격%20OR%20keyword::/추천/내구성)AND(keyword::/카테고리/제품명/%22"+product+"%22)";
+		String myURL = "http://10.10.0.4:8393/api/v10/search/facets?collection=T1_OZIP2022&output=application/json&facet={%22namespace%22:%22keyword%22,%22count%22:%22100%22,%22id%22:%22$._word%22}&query=(keyword::/추천/디자인%20OR%20keyword::/추천/가격%20OR%20keyword::/추천/내구성)%20AND%20(keyword::/카테고리/제품명/%22"+product+"%22)";
 		try {
 			URL url = new URL(myURL);
 			urlConn = url.openConnection();
@@ -33,7 +35,7 @@ public class KeywordServiceImpl implements KeywordService {
 					while((cp = bufReader.read())!= -1) {
 						sb.append((char)cp);
 					}
-					bufReader.close();
+				bufReader.close();
 				}
 			}
 			in.close();
@@ -49,20 +51,17 @@ public class KeywordServiceImpl implements KeywordService {
 		StringBuilder sb = new StringBuilder();
 		URLConnection urlConn = null;
 		InputStreamReader in = null;
-		
-		String myURL = 
-				"http://10.10.0.4:8393/api/v10/search?results=100&collection=T1_OZIP2022&output=application/json&query=(keyword::/추천/디자인%20OR%20keyword::/추천/가격%20OR%20keyword::/추천/내구성)AND(keyword::/카테고리/제품명/%22"+product+"%22)AND(keyword::/%22Part of Speech%22/%22"+thisword+"%22)";
+		String myURL = "http://10.10.0.4:8393/api/v10/search?results=5&collection=T1_OZIP2022&output=application/json&query=(keyword::/추천/디자인%20OR%20keyword::/추천/가격%20OR%20keyword::/추천/내구성)%20AND%20(keyword::/카테고리/제품명/%22"+product+"%22)%20AND%20(keyword::/%22Part%20of%20Speech%22/%22"+thisword+"%22)";
+
+		System.out.println(myURL);
 		try {
 			URL url = new URL(myURL);
 			urlConn = url.openConnection();
 			if(urlConn != null)
 				urlConn.setReadTimeout(60*1000);
-			
 			if(urlConn != null && urlConn.getInputStream() != null) {
 				in = new InputStreamReader(urlConn.getInputStream(),Charset.forName("UTF-8"));
-				
 				BufferedReader bufReader = new BufferedReader(in);
-			
 				if(bufReader != null) {
 					int cp;
 					while((cp = bufReader.read())!= -1) {
@@ -71,14 +70,12 @@ public class KeywordServiceImpl implements KeywordService {
 					bufReader.close();
 				}
 			}
-			in.close();
+			in.close();		
 		}catch(Exception e) {
 			throw new RuntimeException("Exception URL:"+ myURL,e);
 		}
 		String decode = sb.toString();
-		
 		return decode;
 	}
-	
-	
+
 }
